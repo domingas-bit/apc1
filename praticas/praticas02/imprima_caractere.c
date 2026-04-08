65d44db350fbc0979556995cff7d0dfebee2dec4
unknown <2612082008@ACADEMICO.IESB.BR>
2026-04-08 09:49:00 -0300

    feat: conclui pratica02. Fecha #ID

---
+++ praticas/praticas02/imprima_caractere.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main () {
+
+//Imprimir o caractere A, uma quebra de linha \ne o texto Isso aqui eh uma string;//
+
+
+printf("%c\n",A);
+printf("%c\n", "Isso aqui é um texto");
+printf("%s\n",texto);
+    return 0;
+}
---
+++ praticas/praticas02/imprima_inteiros.c
@@ -0,0 +1,15 @@
+ //Imprimir os números 10, -5 e 00004;//
+ # include <stdio.h>
+int main () {
+
+//Imprimir o caractere A, uma quebra de linha \ne o texto Isso aqui eh uma string;//
+int a =10;
+int b = -5;
+int c = 4;
+
+printf("%d\n", a);
+printf("%s\n" b);
+printf("%05d\n",c);
+
+    return 0;
+}
